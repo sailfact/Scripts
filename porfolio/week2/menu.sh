3aab0ea0b40da62ff0e3df87f4c238a015504840
Ross Curley <Ross.Curley@Outlook.com>
2021-07-23 05:17:27 +0000

    2.5 complete

--- porfolio/week2/menu.sh
+++ porfolio/week2/menu.sh
@@ -1,1 +1,36 @@
 #!/bin/bash
+
+# check password
+./passwordCheck.sh
+
+case $? in
+
+    0 )
+        echo "1.Create a folder"
+        echo "2.Copy a folder"
+        echo "3.Set a password"
+
+        read -p "option : " option
+
+        case $option in
+
+            1)
+                ./folderMaker.sh
+            ;;
+            2)
+                ./folderCopier.sh
+            ;;
+            3)
+                ./setPassword.sh
+            ;;
+            *)
+                echo "Invalid Option"
+            ;;
+        
+        esac
+        ;;
+    1 )
+        echo "Incorrect Password"
+        ;;
+
+esac
