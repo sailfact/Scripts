6c3c229be59b6af3b157ab8b5744f3475db5b623
Ross Curley <Ross.Curley@Outlook.com>
2021-07-30 03:11:26 +0000

    2.6 fixed errors.sh

--- porfolio/week2/errors.sh
+++ porfolio/week2/errors.sh
@@ -5,34 +5,21 @@ read -s -p "what's the secret code?"
       
 echo       
 #if the user types in the correct secret, tell them they got it right! 
-      
-if [ "$secret" = "$REPLY" ]; then 
-    echo "You got it right!" 
-      
-    correct=true  
-else      
-    echo "You got it wrong :(" 
-      
-    correct=false  
-fi 
-      
-      
-case $correct in 
-      
-*) 
-      
-    echo "Go Away!" #people who get it wrong need to be told to go away! 
-      
-    ;; 
-      
-true) 
-      
-    echo "you have unlocked the secret menu!" 
-      
-    #TODO: add a secret menu for people in the know. 
-      
-    ;; 
-      
-esac 
-     
-     
+
+if [ "$secret" == "$REPLY" ]; then
+    echo "You got it right!"
+    correct=true
+else
+    echo "you got it wrong :("
+    correct=false
+fi
+
+case $correct in
+    true)
+        echo "you have unlocked the secret menu!"
+        #TODO: add secret menu
+    ;;
+    false)
+        echo "Go Away!" #people who get it wrong need to be told to go away!
+    ;;
+esac
