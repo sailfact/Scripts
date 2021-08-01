dbb1816600ea456ae8cba56de2da3006f1ba3af6
Ross Curley <Ross.Curley@Outlook.com>
2021-08-01 02:38:09 +0000

    3.6 finished mega menu

--- porfolio/week3/megaMenu.sh
+++ porfolio/week3/megaMenu.sh
@@ -4,38 +4,66 @@
 
 # retreive exit code of passwordCheck.sh
 case $? in
-    # sucess
+    # sucsess
     0 )
-        echo "1.Create a folder"
-        echo "2.Copy a folder"
-        echo "3.Set a password"
+        option=0
+        while [ $option -ne 8 ]; do
+            echo "1. Create a folder"
+            echo "2. Copy a folder"
+            echo "3. Set a password"
+            echo "4. Calculator"
+            echo "5. Create Week folders"
+            echo "6. Check file names"
+            echo "7. Download a file"
+            echo "8. Exit"
 
-        read -p "option : " option
+            read -p "option > " option
 
-        case $option in
+            case $option in
 
-            1) 
-                # create folder
-                ./folderMaker.sh
-            ;;
-            2)
-                # copy folder
-                ./folderCopier.sh
-            ;;
-            3)
-                # set passwrod
-                ./setPassword.sh
-            ;;
-            *)  
-                # default
-                echo "Invalid Option"
-            ;;
-        
-        esac
+                1) 
+                    # create folder
+                    ./folderMaker.sh
+                ;;
+                2)
+                    # copy folder
+                    ./folderCopier.sh
+                ;;
+                3)
+                    # set passwrod
+                    ./setPassword.sh
+                ;;
+                4)  
+                    # calculator
+                    ./simplecalc.sh
+                ;;
+                5)
+                    # create week folders
+                    ./megafoldermaker
+                ;;
+                6)
+                    #check files
+                    ./filenames.sh filenames.txt
+                ;;
+                7)
+                    #Download file
+                    ./internet_downloader.sh
+                ;;
+                8)
+                    echo "Exiting"
+                ;;
+                *)  
+                    # default
+                    echo "Invalid Option"
+                ;;
+            
+            esac
+        done
+        exit 0
     ;;
     # fail
     1 )
         echo "Incorrect Password"
+        exit 1
     ;;
-
 esac
