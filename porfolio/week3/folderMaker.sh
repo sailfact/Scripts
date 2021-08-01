7d97ccf65dd3cedd2f73e098f5d76f8f7a5b3c18
Ross Curley <Ross.Curley@Outlook.com>
2021-08-01 02:21:04 +0000

    3.6 added week2 scripts

---
+++ porfolio/week3/folderMaker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+read -p "type name of the folder you would like to create" foldername
+
+mkdir "$foldername"
--- porfolio/week3/megaMenu.sh
+++ porfolio/week3/megaMenu.sh
@@ -1,1 +1,41 @@
 #!/bin/bash
+# check password
+./passwordCheck.sh
+
+# retreive exit code of passwordCheck.sh
+case $? in
+    # sucess
+    0 )
+        echo "1.Create a folder"
+        echo "2.Copy a folder"
+        echo "3.Set a password"
+
+        read -p "option : " option
+
+        case $option in
+
+            1) 
+                # create folder
+                ./folderMaker.sh
+            ;;
+            2)
+                # copy folder
+                ./folderCopier.sh
+            ;;
+            3)
+                # set passwrod
+                ./setPassword.sh
+            ;;
+            *)  
+                # default
+                echo "Invalid Option"
+            ;;
+        
+        esac
+    ;;
+    # fail
+    1 )
+        echo "Incorrect Password"
+    ;;
+
+esac
