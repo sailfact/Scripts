39a2b5d382f096189d63f7df38ca249e1347e032
Ross Curley <rosscurley988@gmail.com>
2021-08-12 11:40:08 +0800

    renamed folder

---
+++ portfolio/week5/regexes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+grep -r echo ~/Scripts/portfolio
+
+# all sed statements
+
+# all lines that start with the letter m
+
+# alllines that contain three digit numbers
+
+# all echo statements with at least three words
+
+# all lines that would make a goo password
