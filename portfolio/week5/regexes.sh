9937a78d0fc87e594d86fa08bc369fd6ca9daca8
Ross Curley <rosscurley988@gmail.com>
2021-08-14 08:35:41 +0800

    5.2 regexes

--- portfolio/week5/regexes.sh
+++ portfolio/week5/regexes.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-grep -r echo ~/Scripts/portfolio
-
 # all sed statements
+grep -r "sed " ~/Scripts/portfolio
+
+echo
+echo
+echo 
 
 # all lines that start with the letter m
+grep -r "^m" ~/Scripts/portfolio
 
-# alllines that contain three digit numbers
+echo 
+echo 
+echo
+
+# all lines that contain three digit numbers
+grep -r ".*\d{3}.*" ~/Scripts/portfolio
+
+echo 
+echo
+echo
 
 # all echo statements with at least three words
+grep -r "echo .*([a-zA-Z]+ ){2,}" ~/Scripts/portfolio 
 
-# all lines that would make a goo password
+echo
+echo
+echo
+
+# all lines that would make a good password
